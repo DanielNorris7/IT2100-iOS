c1d195e6840c69cb414c9317f5df1df7d57bcda7
Daniel Norris <daniel.j.norris@me.com>
2019-01-27 12:59:33 -0500

    Github Username: DanielNorris7 Repo: IT2100-iOS commit Original commit after project creation

---
+++ IT2100DNorris/IT2100DNorris/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IT2100DNorris
+//
+//  Created by Daniel Norris on 1/27/19.
+//  Copyright © 2019 Daniel Norris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
