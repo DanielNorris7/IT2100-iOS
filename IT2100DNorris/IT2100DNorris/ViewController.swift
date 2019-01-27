df2108aea42df113aeac52ed3e9e69b0a35c6a69
Daniel Norris <daniel.j.norris@me.com>
2019-01-27 14:13:46 -0500

    Lab 2 - Completed

--- IT2100DNorris/IT2100DNorris/ViewController.swift
+++ IT2100DNorris/IT2100DNorris/ViewController.swift
@@ -12,9 +12,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var myMessage: UILabel;
+        var myUnusedVariable: String;
+        myMessage=UILabel(frame:CGRect(x: 30.0, y: 50.0, width: 300.0, height: 50.0));
+        myMessage.font=UIFont.systemFont(ofSize: 48.0);
+        myMessage.text="Hello Xcode";
+        myMessage.textColor=UIColor(patternImage: UIImage(named: "Background")!);
+        view.addSubview(myMessage);
+        NSLog("Hello Xcode, Again");
     }
-
-
+    
+    // MARK: -Method for handling memory meanagement
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
 }
 
