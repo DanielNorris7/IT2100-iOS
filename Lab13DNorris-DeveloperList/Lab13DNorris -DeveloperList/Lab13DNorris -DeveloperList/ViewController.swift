933ff2a9271ffca9820318b4a8cdb14f552b1b23
Daniel Norris <42879730+DanielNorris7@users.noreply.github.com>
2019-05-07 19:30:07 -0400

    Lab13 Manual Upload

---
+++ Lab13DNorris-DeveloperList/Lab13DNorris -DeveloperList/Lab13DNorris -DeveloperList/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  Lab13DNorris -DeveloperList
+//
+//  Created by Daniel Norris on 5/4/19.
+//  Copyright © 2019 Daniel Norris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let kSectionCount:Int = 2;
+    let kRedSection: Int = 0;
+    let kBlueSection: Int = 1;
+    let cellID : String = "flowerCell";
+    var flowerMessage: String?;
+
+    //Array with developer names (note: matches image names):
+    let webDevelopers: [String] = ["Al Dente", "Armand Hammer", "Barry Cade", "Cara Van"];
+    
+    let mobileDevelopers: [String] = ["Ginger Ayle", "June Bugg", "Iona Ford", "Sue Flay", "Tim Burr", "Will Wynn"];
+    
+    //Array with developer titles:
+    let webDevelopersTitle: [String] = ["Full Stack", "ASP.NET", "PHP", "Node.js"];
+    
+    let mobileDevelopersTitle: [String] = ["iOS with Swift", "iOS and Android", "iOS with Objective-C", "iOS with Swift and Objective-C", "Android", "Ionic"];
+    
+    let webDevelopersDetails: [String] = ["A full stack developer capable of front-end and back-end development in a Windows environment.", "Back-end developer focused on database applications using a variety of DBMSs.", "Several years experience writing PHP with database connectvity using Linux and Windows.", "Newby developer with great demonstration of skills using Node.js and Angular. Some recent work in React."];
+    
+    let mobileDevelopersDetails: [String] = ["12 apps developed using Swift and Xcode", "Senior app developer with experience in Android and iOS development", "Maintains large code base still in Objective-C. Working to learn Swift.", "Staff expert in Objective-C. Lead efforts to upgrade from Objective-C to Swift.", "Android expert with experience raning to days of Eclipse to current Android Studio versions.", "Prototyping go-to. Uses Ionic to spin up fast prototypes for all apps."];
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2;
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        let numberOfRows = section == 0 ? webDevelopers.count : mobileDevelopers.count;
+        return numberOfRows;
+       
+    }
+    
+    private func tableView(tableView: UITableView, titleForHeader section: Int) -> String? {
+       
+        let headerTitle = section == 0 ? "Web Developer" : "Mobile Developer";
+        return headerTitle;
+    }
+    
+    private func tableView(_ tableView: UITableView, hieghtForHeaderInSection section: Int) -> Int {
+        return 50;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: cellID)! as UITableViewCell;
+        
+        if indexPath.section == 0
+        {
+            cell.textLabel!.text = webDevelopers[indexPath.row];
+            cell.detailTextLabel!.text = webDevelopersTitle[indexPath.row];
+        }
+        else
+        {
+            cell.textLabel!.text = mobileDevelopers[indexPath.row];
+            cell.detailTextLabel!.text = mobileDevelopersTitle[indexPath.row];
+        }
+            
+        let flowerImage: UIImage = UIImage(named: cell.textLabel!.text!)!
+        cell.imageView!.image = flowerImage;
+        
+        return cell;
+        
+    }
+    
+    
+    private func tableView(tableView: UITableView, didSelectRow indexPath: NSIndexPath) {
+        
+        let alertController = UIAlertController(title: "Developer Selected", message: flowerMessage, preferredStyle: UIAlertController.Style.alert);
+        
+        let defaultAction = UIAlertAction(title: "Ok", style: UIAlertAction.Style.cancel, handler: nil);
+        
+        if indexPath.section == 0
+        {
+            flowerMessage = "\(webDevelopersTitle[indexPath.row]) Developer\n\n\(webDevelopers[indexPath.row])\n\n\(webDevelopersDetails[indexPath.row])";
+            
+        }
+        else
+        {
+            flowerMessage = "\(mobileDevelopersTitle[indexPath.row]) Developer\n\n\(mobileDevelopers[indexPath.row])\n\n\(mobileDevelopersDetails[indexPath.row])";
+            
+        }
+        
+        alertController.addAction(defaultAction);
+        
+        present(alertController, animated: true, completion: nil)
+    }
+}
+
---
+++ Lab13DNorris-DeveloperList/Lab13DNorris -DeveloperList/Lab13DNorris -DeveloperListTests/Lab13DNorris__DeveloperListTests.swift
@@ -0,0 +1,34 @@
+//
+//  Lab13DNorris__DeveloperListTests.swift
+//  Lab13DNorris -DeveloperListTests
+//
+//  Created by Daniel Norris on 5/4/19.
+//  Copyright © 2019 Daniel Norris. All rights reserved.
+//
+
+import XCTest
+@testable import Lab13DNorris__DeveloperList
+
+class Lab13DNorris__DeveloperListTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
